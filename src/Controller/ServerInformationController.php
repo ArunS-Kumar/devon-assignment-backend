e24339e1d52f556ab038ac7ee4d1a6d5d861aa40
arun.kumarsekar <arun.kumarsekar@metro-markets.de>
2023-08-26 00:13:40 +0200

    cache into local done

--- src/Controller/ServerInformationController.php
+++ src/Controller/ServerInformationController.php
@@ -6,29 +6,38 @@ use App\DTO\SearchDTO;
 use App\Service\FilterInformationReader;
 use App\Service\ServerInformationReader;
 use PhpOffice\PhpSpreadsheet\Reader\Exception;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class ServerInformationController extends AbstractController
 {
-    public function __construct(
-        public ServerInformationReader $serverInformationReader,
-        public FilterInformationReader $filterInformationReader
-    ) {
-    }
-
     /**
      * @throws Exception
+     * @throws InvalidArgumentException
      */
     #[Route('/server-information', name: 'server-information', methods: 'POST')]
-    public function serverInformation(Request $request, SerializerInterface $serializer): JsonResponse
+    public function serverInformation(
+        Request $request,
+        SerializerInterface $serializer,
+        ServerInformationReader $serverInformationReader,
+        FilterInformationReader $filterInformationReader,
+        CacheInterface $cache
+    ): JsonResponse
     {
         $search = $serializer->deserialize($request->getContent(), SearchDTO::class, 'json');
-        $serverInformation = $this->serverInformationReader->readServerInformation($search);
-        $filterInformation = $this->filterInformationReader->readFilterInformation();
+        $serverInformation = $serverInformationReader->readServerInformation($search);
+
+        // todo -> change cache into raise
+        $filterInformation = $cache->get('find-filter-information', function(ItemInterface $item) use ($filterInformationReader) {
+            $item->expiresAfter(18000);
+            return $filterInformationReader->readFilterInformation();
+        });
 
         // todo -> add collection to pass response
         return new JsonResponse(
